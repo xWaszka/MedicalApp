9684b622c9418efef45469f26b8413fd8741cefb
xWaszka <jestemwaszka@gmail.com>
2025-05-01 19:15:24 +0200

    poprawki

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+ 
+const firebaseConfig = {
+    apiKey: "AIzaSyB3XVapXr0q4O3VV7IJHUF0KjSoAEBWm2s",
+    authDomain: "medicalapp-68cb7.firebaseapp.com",
+    projectId: "medicalapp-68cb7",
+    storageBucket: "medicalapp-68cb7.firebasestorage.app",
+    messagingSenderId: "517067229681",
+    appId: "1:517067229681:web:fa709dad4494b2fbe9dc50"
+};
+ 
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+ 
+export { auth, db }
+ 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
-import store from '@/store'
-import '@/assets/css/main.css'
-import '/dist/output.css'
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App)
-    .use(store)
     .use(router)
     .mount('#app')
