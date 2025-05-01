ac2a8020f4099bea5f1204ae4b61eefe0a6fa3fe
Patryk Pacocha <115879093+pakx2206@users.noreply.github.com>
2025-05-01 18:53:11 +0200

    Update index.js - naprawa zle wrzuconego pliku

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import store from '@/store'
 
 const routes = [
   {
@@ -15,24 +14,24 @@ const routes = [
         path: '/contact',
         name: 'Contact',
         component: () => import('@/views/Contact.vue'),
-      },
+      },      
       {
         path: '/login',
         name: 'Login',
         component: () => import('@/views/Login.vue'),
-        meta: { requiresGuest: true },
-      },
+        meta: { requiresGuest: true }
+      },      
       {
         path: '/register',
         name: 'Register',
         component: () => import('@/views/Register.vue'),
-        meta: { requiresGuest: true },
+        meta: { requiresGuest: true }
       },
       {
         path: '/appointment',
         name: 'Appointment',
         component: () => import('@/views/AppointmentForm.vue'),
-        meta: { requiresAuth: true },
+        meta: { requiresAuth: true }
       },
       {
         path: '/:pathMatch(.*)*',
@@ -42,26 +41,13 @@ const routes = [
       {
         path: '/logout',
         name: 'Logout',
-        beforeEnter: () => {
-          store.dispatch('user/logout')
-          return { name: 'Login' }
-        },
-        meta: { requiresAuth: true },
-      },
-    ],
-  },
+        meta: { requiresAuth: true }
+      }
+    ]
+  }
 ]
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 })
-
-router.beforeEach((to, from, next) => {
-  const user = store.getters['user/getUser']
-  if (to.meta.requiresAuth && !user) return next({ name: 'Login' })
-  if (to.meta.requiresGuest && user) return next({ name: 'Home' })
-  next()
-})
-
 export default router
