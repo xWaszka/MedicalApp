55302c2e64d9e2af913f114aad3380215e4d9257
Patryk Pacocha <115879093+pakx2206@users.noreply.github.com>
2025-05-02 13:44:35 +0200

    Update firebase.js - usuniecie przypadkowo wrzuconego api

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
  
 const firebaseConfig = {
-    apiKey: "AIzaSyB3XVapXr0q4O3VV7IJHUF0KjSoAEBWm2s",
-    authDomain: "medicalapp-68cb7.firebaseapp.com",
-    projectId: "medicalapp-68cb7",
-    storageBucket: "medicalapp-68cb7.firebasestorage.app",
-    messagingSenderId: "517067229681",
-    appId: "1:517067229681:web:fa709dad4494b2fbe9dc50"
+   
 };
  
 const app = initializeApp(firebaseConfig)
